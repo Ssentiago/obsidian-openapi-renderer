ede4c192053b1df0c399fb463c28932b493bae68
Sentiago <arseny317@gmail.com>
2024-07-22 23:53:03 +0300

    changed logger back

--- src/pluginLogging/loggingManager.ts
+++ src/pluginLogging/loggingManager.ts
@@ -1,80 +1,126 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import { OpenAPIPluginContext } from "../contextManager";
-import { OpenAPIRendererPluginLoggerInterface } from "../typing/interfaces";
-import * as SimpleNodeLogger from 'simple-node-logger';
+import * as fs from 'fs'
+import * as path from "path";
+import {OpenAPIPluginContext} from "../core/contextManager";
+import {OpenAPIRendererPluginLoggerInterface} from "../typing/interfaces";
 
 export default class OpenAPIRendererPluginLogger implements OpenAPIRendererPluginLoggerInterface {
-    private readonly logger: SimpleNodeLogger.Logger;
-    private readonly logDir: string;
-    private readonly maxFileSize: number = 1024 * 1024 * 10;
-    private readonly maxLogFiles: number = 7;
+    appContext;
+    private maxFileSize = 1024 * 1024; // Maximum log file size in bytes (1 MB)
+    private readonly logDir;
+
 
     constructor(appContext: OpenAPIPluginContext) {
-        const pluginDir = appContext.plugin.manifest.dir;
+        this.appContext = appContext;
+        const pluginDir = this.appContext.plugin.manifest.dir
         if (pluginDir) {
-            this.logDir = path.join(appContext.app.vault.adapter.basePath, pluginDir, 'logs');
-            this.ensureLogDirExists();
-
-            const logFilePath = path.join(this.logDir, 'logs.log');
-            const options = {
-                logFilePath,
-                maxLogSize: this.maxFileSize,
-                maxLogFiles: this.maxLogFiles,
-            };
-
-            this.logger = SimpleNodeLogger.createSimpleLogger(options);
-
-            this.setupRotation(logFilePath);
+            this.logDir = path.join(this.appContext.app.vault.adapter.basePath, pluginDir, 'logs');
+            this.ensureLogDirExists()
         } else {
-            throw new Error('Plugin directory is not defined');
+            throw new Error('Plugin dir not found');
         }
     }
 
     private ensureLogDirExists() {
-        if (!fs.existsSync(this.logDir)) {
-            fs.mkdirSync(this.logDir, { recursive: true });
-        }
-    }
-
-    private setupRotation(filePath: string) {
-        fs.watchFile(filePath, { interval: 10000 }, () => {
-            this.checkLogSize(filePath);
-        });
-    }
-
-    private checkLogSize(filePath: string) {
-        fs.stat(filePath, (err, stats) => {
+        fs.access(this.logDir, fs.constants.F_OK, (err) => {
             if (err) {
-                this.logger.error('Error checking log file size', err);
-            } else if (stats.size >= this.maxFileSize) {
-                this.rotateLogs(filePath);
+                fs.mkdir(this.logDir, {recursive: true}, (err) => {
+                    if (err) {
+                        console.error('Error creating log directory:', err);
+                    }
+                });
             }
         });
     }
 
-    private rotateLogs(filePath: string) {
-        const rotatedLogPath = path.join(this.logDir, `logs_${Date.now()}.log`);
-        fs.rename(filePath, rotatedLogPath, (err) => {
+    /**
+     * Rotates the log file by creating a new file with a timestamped name and moving the existing log file to it.
+     * This function is used to manage the size of the log file and prevent it from growing indefinitely.
+     *
+     * @returns void
+     */
+    private getLogFilePath() {
+        return path.join(this.logDir, 'logs.txt');
+    }
+
+    private rotateLogs() {
+        const logPath = this.getLogFilePath();
+        const rotatedLogPath = path.join(this.logDir, `logs_${Date.now()}.txt`);
+
+        fs.rename(logPath, rotatedLogPath, (err) => {
             if (err) {
-                this.logger.error('Error rotating log file', err);
+                console.error('Error rotating log file:', err);
             }
         });
     }
 
-    info(message: string, context: object = {}) {
-        this.logger.info(message, context);
+
+    /**
+     * Writes a log entry to the log file.
+     *
+     * @param level - The severity level of the log entry.
+     * @param message - The message to be logged.
+     * @param context - Additional contextual information for the log entry.
+     */
+    private log(level: string, message: string, context = {}) {
+
+        const logEntry = {
+            timestamp: new Date().toLocaleString("ru"),
+            level: level,
+            message: message,
+            context: {...context}
+        };
+
+        const logMessage = JSON.stringify(logEntry) + '\n';
+        const logPath = this.getLogFilePath();
+
+        fs.access(logPath, fs.constants.F_OK, (err) => {
+            if (err) {
+                // The file does not exist, create a new log file
+                fs.writeFile(logPath, logMessage, (err) => {
+                    if (err) {
+                        console.error('Error writing to log file:', err);
+                    }
+                });
+            } else {
+                // The file exists, check the size and add an entry
+                fs.stat(logPath, (err, stats) => {
+                    if (err) {
+                        console.error('Error checking log file size:', err);
+                    } else if (stats.size >= this.maxFileSize) {
+                        // File exceeds size, perform rotation
+                        this.rotateLogs();
+                        fs.writeFile(logPath, logMessage, (err) => {
+                            if (err) {
+                                console.error('Error writing to new log file:', err);
+                            }
+                        });
+                    } else {
+                        // Write to the current log file
+                        fs.appendFile(logPath, logMessage, (err) => {
+                            if (err) {
+                                console.error('Error writing to log file:', err);
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
-    error(message: string, context: object = {}) {
-        this.logger.error(message, context);
+    info(message: string, context = {}) {
+        this.log('INFO', message, context);
     }
 
-    warn(message: string, context: object = {}) {
-        this.logger.warn(message, context);
+    error(message: string, context = {}) {
+        this.log('ERROR', message, context);
     }
 
-    debug(message: string, context: object = {}) {
-        this.logger.debug(message, context);
+    warn(message: string, context = {}) {
+        this.log('WARNING', message, context);
     }
+
+    debug(message: string, context = {}) {
+        this.log('DEBUG', message, context);
+    }
+
 }
