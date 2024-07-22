4ba22eb68f047aefdb4f60fd69c4e2ef16cf8eb1
Sentiago <arseny317@gmail.com>
2024-07-22 17:54:23 +0300

    removes old resources

--- src/assets/shared resources.ts
+++
@@ -1,7 +0,0 @@
-import swaggerUICSS from '../assets/swagger-ui/swagger-ui.module.css'
-import swaggerUIJS from '../assets/swagger-ui/swagger-ui-bundle.module.js'
-import JSYAML from '../assets/js-yaml/js-yaml.module.js'
-
-
-export {swaggerUICSS, swaggerUIJS, JSYAML}
-
