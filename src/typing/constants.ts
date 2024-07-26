306218a061936feecba82773b3889122550f8f65
Sentiago <arseny317@gmail.com>
2024-07-26 23:20:40 +0300

    added CSS_NAME and SwaggerTheme constants

--- src/typing/constants.ts
+++ src/typing/constants.ts
@@ -32,3 +32,14 @@ export const REFRESHER_BUTTON_ID = 'openapi-refresher';
 export const SERVER_BUTTON_ID = 'openapi-renderer-server';
 export const SERVER_ICON_NAME_ON = 'wifi';
 export const SERVER_ICON_NAME_OFF = 'wifi-off';
+
+export const enum CSS_NAME {
+    BaseCSS = 'swagger-ui-base.css',
+    LightThemeCSS = 'swagger-ui-light.css',
+    DarkThemeCSS = 'swagger-ui-dark.css',
+}
+
+export const enum SwaggerUITheme {
+    dark = 'dark',
+    light = 'light',
+}
