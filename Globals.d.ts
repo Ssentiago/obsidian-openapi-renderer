21eabfb0d099347881e00392f778bc40cb163f77
Sentiago <arseny317@gmail.com>
2024-07-30 02:04:26 +0300

    refactor: remove old CSS and JS module declarations; add web-worker module declaration

--- Globals.d.ts
+++ Globals.d.ts
@@ -1,13 +1,4 @@
-// declare module "*.module.css";
-// declare module "*.module.scss";
-
-
-declare module '*.module.css' {
-  const content: string;
-  export default content;
-}
-
-declare module "*.module.js" {
-  const content: string;
-  export default content;
+declare module 'web-worker:*' {
+    const WorkerFactory: new () => Worker;
+    export default WorkerFactory;
 }
