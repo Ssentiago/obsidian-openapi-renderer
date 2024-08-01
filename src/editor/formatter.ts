7c59ccedff12f33e08204ce6ac4c01867bf2faec
Sentiago <arseny317@gmail.com>
2024-08-01 05:38:41 +0300

    feat: add JSON/YAML formatter
    - Format JSON using `JSON.parse` with double indent and `JSON.stringify`
    - Format YAML using `yaml.load` with double indent and `yaml.dump`

---
+++ src/editor/formatter.ts
@@ -0,0 +1,24 @@
+import { SwaggerEditor } from './OpenAPIEditorView';
+import { EditorView } from '@codemirror/view';
+import yaml from 'js-yaml';
+
+export function openAPIFormatter(editor: SwaggerEditor, view: EditorView) {
+    let isJson = editor.file?.extension === 'json';
+    const content = view.state.doc.toString();
+    let formatterContent: string;
+    if (isJson) {
+        formatterContent = JSON.stringify(JSON.parse(content), null, 2);
+    } else {
+        formatterContent = yaml.dump(yaml.load(content), {
+            indent: 2,
+        });
+    }
+    view.dispatch({
+        changes: {
+            from: 0,
+            to: view.state.doc.length,
+            insert: formatterContent,
+        },
+    });
+    editor.plugin.showNotice('Formatted');
+}
