4f2d827fb94bb2430bede90e45bd420d525d57e6
Sentiago <arseny317@gmail.com>
2024-08-06 00:06:19 +0300

    feat: add preview mode to OpenAPI View for Swagger UI rendering from OpenAPI specification

---
+++ src/view/OpenAPI/components/preview/OpenAPI-preview.ts
@@ -0,0 +1,36 @@
+import OpenAPIRendererPlugin from 'core/OpenAPIRendererPlugin';
+import { RESOURCE_NAME } from 'typing/constants';
+import { OpenAPIView } from 'view/OpenAPI/OpenAPI-view';
+import OpenAPIPreviewController from 'view/OpenAPI/components/preview/controllers/preview-controller';
+import { IOpenAPIViewComponent } from 'typing/interfaces';
+
+export default class OpenAPIPreview implements IOpenAPIViewComponent {
+    renderTimeout: NodeJS.Timeout | null = null;
+    currentThemeMode!: string;
+    currentThemeCSS!: RESOURCE_NAME;
+    controller: OpenAPIPreviewController;
+    cachedPreview: null | HTMLDivElement = null;
+
+    constructor(
+        public openAPIView: OpenAPIView,
+        public plugin: OpenAPIRendererPlugin,
+        public contentEl: HTMLElement
+    ) {
+        this.controller = new OpenAPIPreviewController(this);
+    }
+
+    async render(): Promise<void> {
+        this.clear();
+        this.controller.initializePreview();
+        await this.controller.render();
+    }
+
+    clear(): void {
+        this.contentEl.empty();
+        this.openAPIView.controller.clearActions();
+    }
+
+    getComponentData(): string {
+        return this.openAPIView.data;
+    }
+}
