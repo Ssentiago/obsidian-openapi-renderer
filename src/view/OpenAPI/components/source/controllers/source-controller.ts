e021a383fe6189f3c44529a8231e2655a1688d83
Sentiago <arseny317@gmail.com>
2024-08-06 04:15:03 +0300

    feat: add source controller for source mode of OpenAPI View

---
+++ src/view/OpenAPI/components/source/controllers/source-controller.ts
@@ -0,0 +1,30 @@
+import { OpenAPISource } from '../OpenAPI-source';
+import { ThemeController } from './theme-controller';
+import { SourceUtils } from './source-utils';
+
+export class SourceController {
+    themeManager: ThemeController;
+
+    constructor(public editor: OpenAPISource) {
+        this.themeManager = new ThemeController(this);
+        this.initializeTheme();
+        this.setupEventListeners();
+    }
+
+    initializeEditor(): void {
+        this.initializeActions();
+        SourceUtils.initializeEditor(this);
+    }
+
+    setupEventListeners(): void {
+        SourceUtils.setupEventListeners(this);
+    }
+
+    initializeActions(): void {
+        SourceUtils.initializeActions(this);
+    }
+
+    initializeTheme(): void {
+        this.themeManager.initializeTheme();
+    }
+}
