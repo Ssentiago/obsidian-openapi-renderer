38faec7c8ba1208d2ab7b1718b5341417d0e8bcc
Sentiago <arseny317@gmail.com>
2024-08-13 18:36:29 +0300

    refactor: rename interfaces and update data retrieval type
    - Renamed interfaces
    - Updated data retrieval type from `event.payload<something>` to `event.payload.data.<something>`

--- src/indexedDB/worker/db-worker.ts
+++ src/indexedDB/worker/db-worker.ts
@@ -1,6 +1,6 @@
 import { IndexedDB } from '../database/indexedDB';
 import { BaseSpecification } from '../database/specification';
-import { messageType, responseType, WorkerMessage } from '../interfaces';
+import { MessageType, ResponseType, WorkerMessage } from '../interfaces';
 
 const db = new IndexedDB();
 
@@ -9,73 +9,73 @@ self.onmessage = async (event: MessageEvent<WorkerMessage>): Promise<void> => {
     try {
         let id: number;
         switch (type) {
-            case messageType.AddVersion:
-                const spec = new BaseSpecification(payload.spec);
+            case MessageType.AddVersion:
+                const spec = new BaseSpecification(payload.data.spec);
                 await db.add(spec);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                 });
 
                 break;
-            case messageType.GetVersions:
-                const path = payload.path;
+            case MessageType.GetVersions:
+                const path = payload.data.path;
                 if (!path) {
                     return;
                 }
                 const versions = await db.getVersions(path);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: { data: versions },
                 });
                 break;
-            case messageType.GetLastVersion:
-                const lastVersion = await db.getLastVersion(payload.path);
+            case MessageType.GetLastVersion:
+                const lastVersion = await db.getLastVersion(payload.data.path);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: { data: lastVersion },
                 });
                 break;
-            case messageType.DeleteVersion:
-                id = parseInt(payload.id, 10);
+            case MessageType.DeleteVersion:
+                id = parseInt(payload.data.id, 10);
                 await db.deleteVersion(id);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: null,
                 });
                 break;
-            case messageType.RestoreVersion:
-                id = parseInt(payload.id, 10);
+            case MessageType.RestoreVersion:
+                id = parseInt(payload.data.id, 10);
                 await db.restoreVersion(id);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: null,
                 });
                 break;
-            case messageType.DeletePermanently:
-                id = parseInt(payload.id, 10);
+            case MessageType.DeletePermanently:
+                id = parseInt(payload.data.id, 10);
                 await db.deleteVersionPermanently(id);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: null,
                 });
                 break;
-            case messageType.IsNextVersionFull:
-                const fPath = payload.path;
+            case MessageType.IsNextVersionFull:
+                const fPath = payload.data.path;
                 const isNextVersionFull = await db.isNextVersionFull(fPath);
                 self.postMessage({
-                    type: responseType.Success,
+                    type: ResponseType.Success,
                     payload: { data: isNextVersionFull },
                 });
                 break;
             default:
                 self.postMessage({
-                    type: responseType.Error,
+                    type: ResponseType.Error,
                     payload: { message: `Unknown message type: ${type}` },
                 });
         }
     } catch (err: any) {
         self.postMessage({
-            type: responseType.Error,
+            type: ResponseType.Error,
             payload: { message: err.message },
         });
     }
