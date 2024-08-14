e7760f1b7e564ec4def1725b963b8353c7701abd
Sentiago <arseny317@gmail.com>
2024-08-14 04:32:34 +0300

    feat: add React context for state management

---
+++ src/view/OpenAPI Version/components/core/context.tsx
@@ -0,0 +1,69 @@
+import React, { createContext, ReactNode, useContext, useState } from 'react';
+import { Specification } from 'indexedDB/database/specification';
+
+interface SpecificationContextProps {
+    isPreviewMode: boolean;
+    setPreviewMode: React.Dispatch<React.SetStateAction<boolean>>;
+    isDiffMode: boolean;
+    setDiffMode: React.Dispatch<React.SetStateAction<boolean>>;
+    specs: Specification[];
+    setSpecs: React.Dispatch<React.SetStateAction<Specification[]>>;
+    currentSpec: string | Specification | undefined;
+    setCurrentSpec: React.Dispatch<
+        React.SetStateAction<string | Specification | undefined>
+    >;
+    selectedSpecs: Specification[];
+    setSelectedSpecs: React.Dispatch<React.SetStateAction<Specification[]>>;
+    openGroups: Record<string, boolean>;
+    setOpenGroups: React.Dispatch<
+        React.SetStateAction<Record<string, boolean>>
+    >;
+}
+
+const SpecificationContext = createContext<
+    SpecificationContextProps | undefined
+>(undefined);
+
+export const SpecificationProvider: React.FC<{ children: ReactNode }> = ({
+    children,
+}) => {
+    const [isPreviewMode, setPreviewMode] = useState<boolean>(false);
+    const [isDiffMode, setDiffMode] = useState<boolean>(false);
+    const [specs, setSpecs] = useState<Specification[]>([]);
+    const [currentSpec, setCurrentSpec] = useState<
+        string | Specification | undefined
+    >(undefined);
+    const [selectedSpecs, setSelectedSpecs] = useState<Specification[]>([]);
+    const [openGroups, setOpenGroups] = useState<Record<string, boolean>>({});
+
+    return (
+        <SpecificationContext.Provider
+            value={{
+                isPreviewMode,
+                setPreviewMode,
+                isDiffMode,
+                setDiffMode,
+                specs,
+                setSpecs,
+                currentSpec,
+                setCurrentSpec,
+                selectedSpecs,
+                setSelectedSpecs,
+                openGroups,
+                setOpenGroups,
+            }}
+        >
+            {children}
+        </SpecificationContext.Provider>
+    );
+};
+
+export const useSpecificationContext = (): SpecificationContextProps => {
+    const context = useContext(SpecificationContext);
+    if (!context) {
+        throw new Error(
+            'useSpecificationContext must be used within a SpecificationProvider'
+        );
+    }
+    return context;
+};
