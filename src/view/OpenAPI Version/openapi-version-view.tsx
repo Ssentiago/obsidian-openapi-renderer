b34ca312046d030503b99e661d8003184d2c7486
Sentiago <arseny317@gmail.com>
2024-08-14 04:04:52 +0300

    feat: add OpenAPI Version View for managing specification versions
    - Add view for managing OpenAPI specification versioning
    - Implement functionality for adding, deleting, previewing, getting diffs, and restoring versions

---
+++ src/view/OpenAPI Version/openapi-version-view.tsx
@@ -0,0 +1,82 @@
+import { IconName, TextFileView, TFile, WorkspaceLeaf } from 'obsidian';
+import React from 'react';
+import { createRoot, Root } from 'react-dom/client';
+import { Specification } from '../../indexedDB/database/specification';
+import { VersionController } from './controllers/version-controller';
+import OpenAPIRendererPlugin from '../../core/OpenAPIRendererPlugin';
+import { OPENAPI_VERSION_VIEW_TYPE } from '../types';
+import { VersionViewEntry } from './components/entry-component';
+
+export class OpenAPIVersionView extends TextFileView {
+    versions!: Array<Specification>;
+    controller: VersionController;
+    reactRoot!: Root | undefined;
+    plugin: OpenAPIRendererPlugin;
+
+    constructor(leaf: WorkspaceLeaf, plugin: OpenAPIRendererPlugin) {
+        super(leaf);
+        this.plugin = plugin;
+
+        this.controller = new VersionController(this);
+    }
+
+    getViewType(): string {
+        return OPENAPI_VERSION_VIEW_TYPE;
+    }
+
+    getDisplayText(): string {
+        if (this.file) {
+            return `OpenAPI Version View: ${this.file.name}`;
+        }
+        return 'No file open';
+    }
+
+    async onLoadFile(file: TFile): Promise<void> {
+        await super.onLoadFile(file);
+
+        const { contentEl } = this;
+
+        if (!this.file) {
+            contentEl.createDiv({ text: 'No file open' });
+            return;
+        }
+
+        const spec = await this.controller.getVersionHistory();
+        if (!spec) {
+            return;
+        }
+        this.versions = spec;
+
+        contentEl.empty();
+
+        const reactRootElement = contentEl.createDiv();
+
+        this.reactRoot = createRoot(reactRootElement);
+        this.reactRoot.render(
+            <VersionViewEntry specifications={this.versions} view={this} />
+        );
+    }
+
+    async onClose(): Promise<void> {
+        if (this.reactRoot) {
+            this.reactRoot.unmount();
+        }
+        this.controller.workerHelper.terminateWorker();
+    }
+
+    clear(): void {
+        this.data = '';
+    }
+
+    getViewData(): string {
+        return this.data;
+    }
+
+    setViewData(data: string, clear: boolean): void {
+        this.data = data;
+    }
+
+    getIcon(): IconName {
+        return 'file-clock';
+    }
+}
