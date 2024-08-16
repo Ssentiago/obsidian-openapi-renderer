96b19daf558191858b14a0594edde52a78395929
Sentiago <arseny317@gmail.com>
2024-08-17 00:50:30 +0300

    fix: resolve bug with TypeScript import of jsondiffpatch/formatters/html

--- Globals.d.ts
+++ Globals.d.ts
@@ -2,3 +2,17 @@ declare module 'web-worker:*' {
     const WorkerFactory: new () => Worker;
     export default WorkerFactory;
 }
+declare module 'jsondiffpatch/formatters/html' {
+    import { Delta } from 'jsondiffpatch';
+    export default class HtmlFormatter {
+        format(delta: Delta, left?: unknown): string | undefined;
+    }
+
+    export function hideUnchanged(node?: Element, delay?: number): void;
+
+    export function showUnchanged(
+        show?: boolean,
+        node?: Element | null,
+        delay?: number
+    ): void;
+}
