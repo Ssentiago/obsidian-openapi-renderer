8744911a718f7aa97810dc5cca3847209fd13bfd
Sentiago <arseny317@gmail.com>
2024-08-16 21:00:05 +0300

    refactor: remove deprecated setting action

--- src/view/OpenAPI Entry/controllers/entry-controller.ts
+++ src/view/OpenAPI Entry/controllers/entry-controller.ts
@@ -1,14 +1,12 @@
 import { OpenAPIEntryView } from '../OpenAPI-entry-view';
 import { WorkerHelper } from 'indexedDB/helper';
 import { EntryViewData, MessageType, ResponseType } from 'indexedDB/interfaces';
-import { SettingsOpenapiEntryModal } from '../modals/settings-openapi-entry-modal';
 
 export class EntryController {
     helper: WorkerHelper;
 
     constructor(public view: OpenAPIEntryView) {
         this.helper = new WorkerHelper();
-        this.initializeActions();
     }
 
     async getEntryViewData(): Promise<EntryViewData[]> {
@@ -20,14 +18,5 @@ export class EntryController {
             return response.payload.data as Array<EntryViewData>;
         }
         return [];
-    }
-
-    initializeActions(): void {
-        this.view.addAction('gear', 'Settings', () => {
-            new SettingsOpenapiEntryModal(
-                this.view.app,
-                this.view.plugin
-            ).open();
-        });
     }
 }
