7152d92e5dea818dee09d43fd6b91803773ef761
Sentiago <arseny317@gmail.com>
2024-08-25 17:22:00 +0300

    feat: remove restore button from Draft component; add RestoredLabel to render if there is a restored version in shared context

--- src/view/OpenAPI Version/components/draft-version/draft-version-component.tsx
+++ src/view/OpenAPI Version/components/draft-version/draft-version-component.tsx
@@ -1,47 +1,53 @@
 import React from 'react';
+import { FaExternalLinkAlt, FaEye, FaSave } from 'react-icons/fa';
+import styled from 'styled-components';
+import { useSpecificationContext } from '../core/context';
 import {
     ButtonGroup,
     Card,
     OpenInOpenAPIButton,
     PreviewButton,
-    RestoreButton,
     SaveButton,
     Title,
 } from '../styled/styled-components';
-import {
-    FaCalendarAlt,
-    FaExternalLinkAlt,
-    FaEye,
-    FaSave,
-} from 'react-icons/fa';
-import styled from 'styled-components';
 
 const CurrentVersionContainer = styled.div`
     margin-bottom: 30px;
+`;
+
+const RestoredLabel = styled.div`
+    background-color: var(--color-base-25);
+    color: var(--color-blue);
+    padding: 8px 16px;
+    border-radius: 8px;
+    font-size: 14px;
+    font-weight: bold;
+    display: inline-block;
+    box-shadow: 0px 2px 4px rgba(var(--mono-rgb-100), 0.1);
+    margin-top: 16px;
 `;
 
 const DraftVersionComponent: React.FC<{
     onPreview: () => void;
     onSave: () => Promise<void>;
     onSaveSuccess: () => void;
-    onRestoreTo: () => void;
     onOpenInOpenAPIView: () => void;
-}> = ({
-    onPreview,
-    onSave,
-    onSaveSuccess,
-    onRestoreTo,
-    onOpenInOpenAPIView,
-}) => {
+}> = ({ onPreview, onSave, onSaveSuccess, onOpenInOpenAPIView }) => {
     const handleSave = async (): Promise<void> => {
         await onSave();
         onSaveSuccess();
     };
+    const { restored } = useSpecificationContext();
 
     return (
         <CurrentVersionContainer>
             <Card $isCurrent={true}>
                 <Title>Draft version</Title>
+                {restored && (
+                    <RestoredLabel>
+                        Restored from version {restored.version}
+                    </RestoredLabel>
+                )}
                 <ButtonGroup>
                     <PreviewButton onClick={onPreview}>
                         <FaEye style={{ color: 'var(--icon-color)' }} />
@@ -51,10 +57,6 @@ const DraftVersionComponent: React.FC<{
                         <FaSave style={{ color: 'var(--icon-color)' }} />
                         Save draft version
                     </SaveButton>
-                    <RestoreButton onClick={onRestoreTo}>
-                        <FaCalendarAlt style={{ color: 'var(--icon-color)' }} />
-                        Restore to...
-                    </RestoreButton>
                     <OpenInOpenAPIButton onClick={onOpenInOpenAPIView}>
                         <FaExternalLinkAlt
                             style={{ color: 'var(--icon-color)' }}
