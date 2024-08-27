25a8f3a5c5504fc33ed94f6a0bfebd2f4169533c
Sentiago <arseny317@gmail.com>
2024-08-27 04:28:29 +0300

    feat: add new pre-save checks for save version modal
    - Version cannot contain a dot
    - Version must be greater than previous version

--- src/view/OpenAPI Version/modals/save-current-version-modal.ts
+++ src/view/OpenAPI Version/modals/save-current-version-modal.ts
@@ -1,4 +1,5 @@
 import { App, Modal, Notice, Setting } from 'obsidian';
+import { Specification } from '../../../indexedDB/database/specification';
 
 export interface FormData {
     priority?: string;
@@ -10,28 +11,19 @@ export interface FormData {
     specVersion?: string;
 }
 
-function getBaseFormData() {
-    return {
-        priority: undefined,
-        status: undefined,
-        changeType: undefined,
-        description: undefined,
-        tags: undefined,
-        specName: undefined,
-        specVersion: undefined,
-    };
-}
-
 export class SaveCurrentVersionModal extends Modal {
-    formData: FormData = getBaseFormData();
-    private submitted!: boolean;
+    formData: FormData = this.getBaseFormData();
+    submitted!: boolean;
 
-    constructor(app: App) {
+    constructor(
+        app: App,
+        public versions: Specification[]
+    ) {
         super(app);
         this.titleEl.textContent = 'Save current version';
     }
 
-    async onOpen() {
+    async onOpen(): Promise<void> {
         const { contentEl } = this;
 
         new Setting(contentEl)
@@ -72,109 +64,112 @@ export class SaveCurrentVersionModal extends Modal {
             .setClass('newlines')
             .setClass('required-field');
 
-        new Setting(contentEl)
-            .setName('Tags')
-            .setDesc(
-                'Tags associated with this version for easier searching and filtering.'
-            )
-            .addText((input) => {
-                input.onChange((value) => {
-                    this.formData.tags = value;
-                });
-            })
-            .addExtraButton((button) =>
-                button
-                    .setIcon('info')
-                    .setTooltip(
-                        'Tags can help categorize and find this version more easily.'
-                    )
-            );
-
-        new Setting(contentEl)
-            .setName('Description')
-            .setDesc('Describe the changes made in this version.')
-            .addTextArea((input) => {
-                input.onChange((value) => {
-                    this.formData.description = value;
-                });
-            })
-            .addExtraButton((button) =>
-                button
-                    .setIcon('info')
-                    .setTooltip(
-                        'Provide a summary of the changes and improvements in this version.'
-                    )
-            );
-
-        new Setting(contentEl)
-            .setName('Change Type')
-            .setDesc(
-                'Specify the type of changes made, e.g., addition, removal, fix.'
-            )
-            .addDropdown((dropdown) => {
-                dropdown.addOption('addition', 'Addition');
-                dropdown.addOption('removal', 'Removal');
-                dropdown.addOption('fix', 'Fix');
-                dropdown.addOption('update', 'Update');
-                dropdown.onChange((value) => {
-                    this.formData.changeType = value;
-                });
-            })
-            .addExtraButton((button) =>
-                button
-                    .setIcon('info')
-                    .setTooltip(
-                        'Select the type of changes made in this version.'
-                    )
-            );
-
-        new Setting(contentEl)
-            .setName('Status')
-            .setDesc(
-                'Specify the status of this version, e.g., draft, published, testing.'
-            )
-            .addDropdown((dropdown) => {
-                dropdown.addOption('draft', 'Draft');
-                dropdown.addOption('published', 'Published');
-                dropdown.addOption('testing', 'Testing');
-                dropdown.onChange((value) => {
-                    this.formData.status = value;
-                });
-            })
-            .addExtraButton((button) =>
-                button
-                    .setIcon('info')
-                    .setTooltip('Select the current status of this version.')
-            );
-
-        new Setting(contentEl)
-            .setName('Priority')
-            .setDesc('Select the priority of changes in this version.')
-            .addDropdown((dropdown) => {
-                dropdown.addOption('low', 'Low');
-                dropdown.addOption('medium', 'Medium');
-                dropdown.addOption('high', 'High');
-                dropdown.onChange((value) => {
-                    this.formData.priority = value;
-                });
-            })
-            .addExtraButton((button) =>
-                button
-                    .setIcon('info')
-                    .setTooltip(
-                        'Choose the priority level for the changes in this version.'
-                    )
-            );
+        // new Setting(contentEl)
+        //     .setName('Tags')
+        //     .setDesc(
+        //         'Tags associated with this version for easier searching and filtering.'
+        //     )
+        //     .addText((input) => {
+        //         input.onChange((value) => {
+        //             this.formData.tags = value;
+        //         });
+        //     })
+        //     .addExtraButton((button) =>
+        //         button
+        //             .setIcon('info')
+        //             .setTooltip(
+        //                 'Tags can help categorize and find this version more easily.'
+        //             )
+        //     );
+        //
+        // new Setting(contentEl)
+        //     .setName('Description')
+        //     .setDesc('Describe the changes made in this version.')
+        //     .addTextArea((input) => {
+        //         input.onChange((value) => {
+        //             this.formData.description = value;
+        //         });
+        //     })
+        //     .addExtraButton((button) =>
+        //         button
+        //             .setIcon('info')
+        //             .setTooltip(
+        //                 'Provide a summary of the changes and improvements in this version.'
+        //             )
+        //     );
+        //
+        // new Setting(contentEl)
+        //     .setName('Change Type')
+        //     .setDesc(
+        //         'Specify the type of changes made, e.g., addition, removal, fix.'
+        //     )
+        //     .addDropdown((dropdown) => {
+        //         dropdown.addOption('addition', 'Addition');
+        //         dropdown.addOption('removal', 'Removal');
+        //         dropdown.addOption('fix', 'Fix');
+        //         dropdown.addOption('update', 'Update');
+        //         dropdown.onChange((value) => {
+        //             this.formData.changeType = value;
+        //         });
+        //     })
+        //     .addExtraButton((button) =>
+        //         button
+        //             .setIcon('info')
+        //             .setTooltip(
+        //                 'Select the type of changes made in this version.'
+        //             )
+        //     );
+        //
+        // new Setting(contentEl)
+        //     .setName('Status')
+        //     .setDesc(
+        //         'Specify the status of this version, e.g., draft, published, testing.'
+        //     )
+        //     .addDropdown((dropdown) => {
+        //         dropdown.addOption('draft', 'Draft');
+        //         dropdown.addOption('published', 'Published');
+        //         dropdown.addOption('testing', 'Testing');
+        //         dropdown.onChange((value) => {
+        //             this.formData.status = value;
+        //         });
+        //     })
+        //     .addExtraButton((button) =>
+        //         button
+        //             .setIcon('info')
+        //             .setTooltip('Select the current status of this version.')
+        //     );
+        //
+        // new Setting(contentEl)
+        //     .setName('Priority')
+        //     .setDesc('Select the priority of changes in this version.')
+        //     .addDropdown((dropdown) => {
+        //         dropdown.addOption('low', 'Low');
+        //         dropdown.addOption('medium', 'Medium');
+        //         dropdown.addOption('high', 'High');
+        //         dropdown.onChange((value) => {
+        //             this.formData.priority = value;
+        //         });
+        //     })
+        //     .addExtraButton((button) =>
+        //         button
+        //             .setIcon('info')
+        //             .setTooltip(
+        //                 'Choose the priority level for the changes in this version.'
+        //             )
+        //     );
 
         new Setting(contentEl).addButton((button) => {
             button.setButtonText('Save').onClick(() => {
-                console.log(this.formData);
                 if (
                     this.formData.specVersion === undefined ||
                     this.formData.specName === undefined
                 ) {
                     new Notice('Please fill in all required fields.');
-                    this.submitted = false;
+                    return;
+                }
+
+                if (this.formData.specName.search(/\./) !== -1) {
+                    new Notice("Name should not contain '.'");
                     return;
                 }
 
@@ -184,16 +179,71 @@ export class SaveCurrentVersionModal extends Modal {
                     );
                     return;
                 }
+                const lastVersion =
+                    this.versions.length > 0 ? this.versions.at(-1) : undefined;
+                if (!lastVersion) {
+                    this.submitted = true;
+                    this.close();
+                    return;
+                }
+                if (
+                    !this.isValidVersion(
+                        this.formData.specVersion,
+                        lastVersion.version
+                    )
+                ) {
+                    new Notice(
+                        'New version must be greater than the previous version.'
+                    );
+                    return;
+                }
+
                 this.submitted = true;
                 this.close();
             });
         });
     }
 
+    private isValidVersion(
+        newVersion: string,
+        previousVersion: string
+    ): boolean {
+        const parseVersion = (version: string): number[] =>
+            version.split('.').map(Number);
+
+        const [newMajor, newMinor, newPatch] = parseVersion(newVersion);
+        const [prevMajor, prevMinor, prevPatch] = parseVersion(previousVersion);
+
+        return (
+            newMajor > prevMajor ||
+            (newMajor === prevMajor && newMinor > prevMinor) ||
+            (newMajor === prevMajor &&
+                newMinor === prevMinor &&
+                newPatch > prevPatch)
+        );
+    }
+
+    getBaseFormData(): {
+        specVersion: undefined;
+        specName: undefined;
+        changeType: undefined;
+        description: undefined;
+        priority: undefined;
+        status: undefined;
+        tags: undefined;
+    } {
+        return {
+            priority: undefined,
+            status: undefined,
+            changeType: undefined,
+            description: undefined,
+            tags: undefined,
+            specName: undefined,
+            specVersion: undefined,
+        };
+    }
+
     onClose(): void {
-        if (!this.submitted) {
-            this.formData = getBaseFormData();
-        }
         this.contentEl.empty();
         super.onClose();
     }
