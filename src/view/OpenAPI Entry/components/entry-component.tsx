bedcae371b10ba7e1b832e49e487fc4c180cc8ed
Sentiago <arseny317@gmail.com>
2024-08-27 05:03:07 +0300

    refactor: change specData signature

--- src/view/OpenAPI Entry/components/entry-component.tsx
+++ src/view/OpenAPI Entry/components/entry-component.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { EntryProvider } from './core/context';
-import { OpenAPIEntryView } from '../OpenAPI-entry-view';
 import { EntryViewData } from '../../../indexedDB/interfaces';
+import { OpenAPIEntryView } from '../OpenAPI-entry-view';
+import { EntryProvider } from './core/context';
 import EntryViewComponent from './entry-view/entry-view-component';
 import { SettingsComponent } from './settings-component/settings=component';
 
 const EntryViewEntry: React.FC<{
     view: OpenAPIEntryView;
-    specData: EntryViewData[];
+    specData: EntryViewData;
 }> = ({ specData, view }) => (
     <EntryProvider>
         <SettingsComponent view={view} />
