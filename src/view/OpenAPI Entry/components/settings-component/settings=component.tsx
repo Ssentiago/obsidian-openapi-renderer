050bcd9c90467940a3548841b68662a123e0a69e
Sentiago <arseny317@gmail.com>
2024-08-27 05:03:41 +0300

    fix: resolve bug with bad react setState

--- src/view/OpenAPI Entry/components/settings-component/settings=component.tsx
+++ src/view/OpenAPI Entry/components/settings-component/settings=component.tsx
@@ -1,16 +1,16 @@
 import React, { useEffect } from 'react';
-import { OpenAPIEntryView } from '../../OpenAPI-entry-view';
-import { useEntryContext } from '../core/context';
 import { eventID } from '../../../../typing/constants';
 import { ChangeGridColumnsStateEvent } from '../../../../typing/interfaces';
+import { OpenAPIEntryView } from '../../OpenAPI-entry-view';
+import { useEntryContext } from '../core/context';
 
 export const SettingsComponent: React.FC<{
     view: OpenAPIEntryView;
 }> = ({ view }) => {
     const { columnValue, setColumnValue } = useEntryContext();
-    setColumnValue(view.plugin.settings.OpenAPIEntryGridColumns);
 
     useEffect(() => {
+        setColumnValue(view.plugin.settings.OpenAPIEntryGridColumns);
         view.plugin.observer.subscribe(
             view.app.workspace,
             eventID.ChangeGridColumnsState,
