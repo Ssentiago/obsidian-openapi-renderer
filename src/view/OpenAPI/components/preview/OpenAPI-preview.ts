19ec1276615586a068639502af62ed6505ac8fdd
Sentiago <arseny317@gmail.com>
2024-08-29 21:57:59 +0300

    refactor: remove cachedPreview field

--- src/view/OpenAPI/components/preview/OpenAPI-preview.ts
+++ src/view/OpenAPI/components/preview/OpenAPI-preview.ts
@@ -1,15 +1,14 @@
 import OpenAPIRendererPlugin from 'core/OpenAPIRendererPlugin';
 import { RESOURCE_NAME } from 'typing/constants';
-import { OpenAPIView } from 'view/OpenAPI/OpenAPI-view';
-import OpenAPIPreviewController from 'view/OpenAPI/components/preview/controllers/preview-controller';
 import { IOpenAPIViewComponent } from 'typing/interfaces';
+import OpenAPIPreviewController from 'view/OpenAPI/components/preview/controllers/preview-controller';
+import { OpenAPIView } from 'view/OpenAPI/OpenAPI-view';
 
 export default class OpenAPIPreview implements IOpenAPIViewComponent {
     renderTimeout: NodeJS.Timeout | null = null;
     currentThemeMode!: string;
     currentThemeCSS!: RESOURCE_NAME;
     controller: OpenAPIPreviewController;
-    cachedPreview: null | HTMLDivElement = null;
 
     constructor(
         public openAPIView: OpenAPIView,
@@ -30,7 +29,6 @@ export default class OpenAPIPreview implements IOpenAPIViewComponent {
 
     clear(): void {
         this.contentEl.empty();
-        this.cachedPreview = null;
     }
 
     close(): void {
