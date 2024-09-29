5f0b81ca3cebd06165337344a7ddec5c42a93f60
Sentiago <arseny317@gmail.com>
2024-09-29 17:15:47 +0300

    refactor: reorganize core folder structure
    - Restructured into three modules: plugin, resource management, and state checkout

---
+++ src/core/openapi-renderer-plugin .ts
@@ -0,0 +1,214 @@
+import {
+    EventObserver,
+    EventPublisher,
+} from 'events-management/events-management';
+import Export from 'export/export';
+import { Notice, Plugin, WorkspaceLeaf } from 'obsidian';
+import { OpenAPISettingTab } from 'settings/settings';
+import { OpenAPIView } from 'view/OpenAPI/OpenAPI-view';
+import { eventID } from '../events-management/typing/constants';
+import { PowerOffEvent } from '../events-management/typing/interfaces';
+import { FileWatcher } from '../filewatcher/filewatcher';
+import GithubClient from '../github/github-client';
+import { WorkerHelper } from '../indexedDB/worker/helper';
+import LoggingManager from '../logging/logging-manager';
+import SettingsManager, {
+    DEFAULT_SETTINGS_Interface,
+} from '../settings/settingsManager';
+import { OpenAPIEntryView } from '../view/OpenAPI Entry/OpenAPI-entry-view';
+import { OpenAPIVersionView } from '../view/OpenAPI Version/openapi-version-view';
+import {
+    OPENAPI_ENTRY_VIEW_TYPE,
+    OPENAPI_VERSION_VIEW_TYPE,
+    OPENAPI_VIEW_TYPE,
+} from '../view/typing/types';
+import ResourceManager from './resource-manager';
+import StateChecker, { State } from './state-checker';
+
+export default class OpenAPIRendererPlugin extends Plugin {
+    settings!: DEFAULT_SETTINGS_Interface;
+    settingsTab!: OpenAPISettingTab;
+    logger!: LoggingManager;
+    publisher!: EventPublisher;
+    observer!: EventObserver;
+    githubClient!: GithubClient;
+    settingsManager!: SettingsManager;
+    pluginUtils!: StateChecker;
+    stateChecker!: StateChecker;
+    resourceManager!: ResourceManager;
+    export!: Export;
+    fileWatcher!: FileWatcher;
+    workerHelper!: WorkerHelper;
+
+    /**
+     * Lifecycle method called when the plugin is loaded.
+     *
+     * This method initializes the plugin and all its components.
+     *
+     * @returns A promise that resolves when the plugin has been initialized.
+     */
+    async onload(): Promise<void> {
+        await this.initializePlugin();
+    }
+
+    /**
+     * Lifecycle method called when the plugin is unloaded.
+     *
+     * This method cleans up after the plugin by publishing a `PowerOffEvent` to notify other components that the plugin
+     * is no longer active.
+     *
+     * @returns A promise that resolves when the plugin has been unloaded.
+     */
+    async onunload(): Promise<void> {
+        const event = {
+            eventID: eventID.PowerOff,
+            timestamp: new Date(),
+            emitter: this.app.workspace,
+        } as PowerOffEvent;
+        this.publisher.publish(event);
+    }
+
+    /**
+     * Initializes the plugin by creating and loading core components, setting up event listeners,
+     * establishing networking connections, creating UI components, and initializing utility classes.
+     *
+     * This method is called when the plugin is loaded.
+     *
+     * @returns A promise that resolves when the plugin has been initialized.
+     */
+    private async initializePlugin(): Promise<void> {
+        await this.initializeCore();
+        this.initializeEventSystem();
+        await this.initializeNetworking();
+        await this.initializeUI();
+        await this.initializeUtilities();
+        this.addSettingTab(new OpenAPISettingTab(this.app, this));
+    }
+
+    /**
+     * Initializes the core components of the plugin.
+     *
+     * This method creates and initializes the settings manager, logger, and resource manager.
+     *
+     * @returns A promise that resolves when the core components have been initialized.
+     */
+    private async initializeCore(): Promise<void> {
+        this.settingsManager = new SettingsManager(this);
+        await this.settingsManager.loadSettings();
+        this.logger = new LoggingManager(this);
+        this.resourceManager = new ResourceManager(this.app, this);
+    }
+
+    /**
+     * Initializes the event system components of the plugin.
+     *
+     * This method creates and initializes the event publisher and observer.
+     *
+     * @private
+     */
+    private initializeEventSystem(): void {
+        this.publisher = new EventPublisher(this);
+        this.observer = new EventObserver(this);
+    }
+
+    /**
+     * Initializes the networking components of the plugin.
+     *
+     * This method creates a new instance of `GithubClient` and assigns it to the `githubClient` property.
+     *
+     * @returns A promise that resolves when the networking components have been initialized.
+     */
+    private async initializeNetworking(): Promise<void> {
+        this.githubClient = new GithubClient(this);
+    }
+
+    /**
+     * Initializes the UI components of the plugin.
+     *
+     * This method registers three views: `OpenAPIView`, `OpenAPIVersionView`, and `OpenAPIEntryView`.
+     * It also registers extensions for YAML, YML, and JSON files so that they can be opened with the
+     * `OpenAPIView` by default.
+     *
+     * Additionally, this method adds a ribbon icon for opening the `OpenAPIEntryView` and a command
+     * for opening the `OpenAPIEntryView` from the command palette.
+     *
+     * @returns A promise that resolves when the UI components have been initialized.
+     */
+    private async initializeUI(): Promise<void> {
+        this.registerView(
+            OPENAPI_VIEW_TYPE,
+            (leaf) => new OpenAPIView(leaf, this)
+        );
+
+        this.registerExtensions(['yaml', 'yml', 'json'], OPENAPI_VIEW_TYPE);
+
+        this.registerView(
+            OPENAPI_VERSION_VIEW_TYPE,
+            (leaf: WorkspaceLeaf) => new OpenAPIVersionView(leaf, this)
+        );
+
+        this.registerView(
+            OPENAPI_ENTRY_VIEW_TYPE,
+            (leaf: WorkspaceLeaf) => new OpenAPIEntryView(leaf, this)
+        );
+
+        this.addRibbonIcon(
+            'file-search-2',
+            'Open OpenAPI Entry View',
+            async () => {
+                const leaf = this.app.workspace.getLeaf(true);
+                await leaf.setViewState({
+                    type: OPENAPI_ENTRY_VIEW_TYPE,
+                    active: true,
+                    state: {},
+                });
+                this.app.workspace.revealLeaf(leaf);
+            }
+        );
+
+        this.addCommand({
+            id: 'openapi-renderer-open-openapi-entry',
+            name: 'OpenAPI Entry',
+            icon: 'file-search-2',
+            callback: async () => {
+                const leaf = this.app.workspace.getLeaf(true);
+                await leaf.setViewState({
+                    type: OPENAPI_ENTRY_VIEW_TYPE,
+                    active: true,
+                    state: {},
+                });
+                this.app.workspace.revealLeaf(leaf);
+            },
+        });
+    }
+
+    /**
+     * Initializes the utility components of the plugin.
+     *
+     * This method creates instances of several utility classes:
+     * - `StateChecker`: This utility checks the state of the plugin, determining if it is being launched for the first time or if there are updates available. It shows appropriate notifications based on its findings.
+     * - `FileWatcher`: This utility monitors file changes in the workspace, allowing the plugin to respond to file renames
+     * - `Export`: This utility handles the export functionality of the plugin and is utilized in views to allow users to export specifications
+     *
+     * It also checks the plugin's state by invoking `checkState` on the `StateChecker`.
+     *
+     * @returns A promise that resolves when all utility components have been initialized.
+     */
+    private async initializeUtilities(): Promise<void> {
+        this.stateChecker = new StateChecker(this);
+        await this.stateChecker.checkState();
+        this.fileWatcher = new FileWatcher(this);
+        this.export = new Export(this);
+        this.workerHelper = new WorkerHelper();
+    }
+
+    /**
+     * Displays a notice to the user with the given message.
+     *
+     * @param message The message to be displayed to the user.
+     * @param duration The duration, in milliseconds, that the notice should be displayed.
+     */
+    showNotice(message: string, duration?: number): void {
+        new Notice(message, duration);
+    }
+}
---
+++ src/core/resource-manager.ts
@@ -0,0 +1,135 @@
+import { App } from 'obsidian';
+import path from 'path';
+import { SwaggerUIBundle } from '../view/typing/swagger-ui-typings';
+import OpenAPIRendererPlugin from './openapi-renderer-plugin ';
+
+export default class ResourceManager {
+    private resourceCache: Map<string, string> = new Map();
+    private assetsPath!: string;
+    public swaggerUIBundle: SwaggerUIBundle | null = null;
+
+    constructor(
+        public app: App,
+        public plugin: OpenAPIRendererPlugin
+    ) {
+        this.initializeAssetsPath().catch((err: any) => {
+            this.plugin.showNotice(err.message);
+            throw err;
+        });
+    }
+
+    /**
+     * Initialize the path to the assets directory.
+     *
+     * @returns {Promise<void>} - A promise that resolves when the assets path is set.
+     * @throws {Error} - Throws an error if the plugin directory is not found.
+     */
+    async initializeAssetsPath(): Promise<void> {
+        const pluginDir = this.plugin.manifest.dir;
+        if (!pluginDir) {
+            throw new Error('No plugin directory found');
+        }
+
+        this.assetsPath = path.join(pluginDir, 'assets');
+    }
+
+    /**
+     * Get the content of a resource file by its path.
+     *
+     * This method uses a cache to store the resource content. If the resource
+     * is not found in the cache, it is read from the file system and stored in
+     * the cache.
+     *
+     * @param resourcePath - The path to the resource file.
+     * @returns {Promise<string>} - A promise that resolves to the resource content.
+     * @throws {Error} - If the resource file is not found
+     */
+    private async getResource(resourcePath: string): Promise<string> {
+        const cachedResource = this.resourceCache.get(resourcePath);
+        if (cachedResource) {
+            return cachedResource;
+        }
+
+        try {
+            const resource = await this.app.vault.adapter.read(resourcePath);
+            this.resourceCache.set(resourcePath, resource);
+            return resource;
+        } catch (err: any) {
+            this.plugin.logger.error(
+                `Error while reading file in view: ${err.message}`
+            );
+            throw new Error(
+                'Error while reading a resource file. Try re-download it'
+            );
+        }
+    }
+
+    /**
+     * Gets the content of a CSS file by its name.
+     *
+     * The folder for the CSS file is determined as follows:
+     * - If the CSS file is `visual-diff.css`, the folder is `jsondiffpatch`.
+     * - Otherwise, the folder is `swagger-ui`.
+     *
+     * @param cssName - The name of the CSS file.
+     * @returns {Promise<string>} - A promise that resolves to the content of the CSS file.
+     * @throws {Error} - If the CSS file is not found
+     */
+    async getCSS(cssName: string): Promise<string> {
+        let folder: string;
+        if (cssName === 'visual-diff.css') {
+            folder = 'jsondiffpatch';
+        } else {
+            folder = 'swagger-ui';
+        }
+        const cssPath = path.join(this.assetsPath, folder, cssName);
+
+        return this.getResource(cssPath);
+    }
+
+    /**
+     * Initializes the Swagger UI bundle.
+     *
+     * This method reads the Swagger UI bundle from the plugin's assets directory and
+     * initializes it. The bundle is stored in the `swaggerUIBundle` property.
+     *
+     * If the bundle has already been initialized, this method does nothing.
+     *
+     * If the bundle could not be initialized, an error is logged and a notice is shown
+     * to the user.
+     *
+     * @returns {Promise<void>} - A promise that resolves when the bundle has been initialized.
+     */
+    async initSwaggerUIBundle(): Promise<void> {
+        if (this.swaggerUIBundle) {
+            return;
+        }
+        const pluginDir = this.plugin.manifest.dir;
+        if (!pluginDir) {
+            throw new Error('No plugin dir found');
+        }
+
+        const assetsPath = path.join(
+            pluginDir,
+            'assets',
+            'swagger-ui',
+            'swagger-ui-bundle.js'
+        );
+
+        try {
+            const swaggerContent =
+                await this.plugin.app.vault.adapter.read(assetsPath);
+            this.swaggerUIBundle = new Function(
+                `${swaggerContent}
+                    return SwaggerUIBundle;`
+            )();
+        } catch (error: any) {
+            this.plugin.logger.error(
+                `Error initializing SwaggerUIBundle: ${error.message}`
+            );
+            this.plugin.showNotice(
+                'Failed to initialize SwaggerUI. Please check the logs for details.'
+            );
+        }
+    }
+}
