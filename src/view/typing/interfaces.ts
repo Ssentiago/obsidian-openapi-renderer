8e0d1ace02f076e27bca7e09e56a901befcb0830
Sentiago <arseny317@gmail.com>
2024-10-09 23:29:28 +0300

    feat: update and add new interfaces for OpenAPI components
    - Added new interface for `ThemeController`
    - Added property `controller.themeController` to `OpenAPIViewComponent`. Now all components must have this controller in their own controller property
    - Added `show` method to `OpenAPIViewComponent`
    - Added new interfaces `PreviewComponent` and `SourceComponent`, both inheriting from `OpenAPIViewComponent`. `SourceComponent` also adds a `clear` method

--- src/view/typing/interfaces.ts
+++ src/view/typing/interfaces.ts
@@ -1,11 +1,32 @@
-export interface IOpenAPIViewComponent {
+interface ThemeController {
+    initializeTheme: () => void;
+    toggleThemeFollowObsidian: () => void;
+    syncWithObsidian: () => Promise<void>;
+    toggleThemeManually: () => void;
+    requestThemeUpdate: () => Promise<void>;
+    themeButtonIcon: 'moon' | 'sun';
+    themeUpdate: () => Promise<void>;
+}
+export interface OpenAPIViewComponent {
+    controller: {
+        themeController: ThemeController;
+    };
+
+    themeMode: string;
+
     render(): void;
 
-    hide(): void;
+    show(): void;
 
-    clear(): void;
+    hide(): void;
 
     close(): void;
 
     getComponentData(): string;
 }
+
+export interface SourceComponent extends OpenAPIViewComponent {
+    clear(): void;
+}
+
+export interface PreviewComponent extends OpenAPIViewComponent {}
