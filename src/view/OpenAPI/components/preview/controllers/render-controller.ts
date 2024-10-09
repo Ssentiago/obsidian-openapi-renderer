911ee3e166bbbec40919e97545544756da268eb5
Sentiago <arseny317@gmail.com>
2024-10-09 23:58:48 +0300

    feat: add method for updating preview based on updated specification

--- src/view/OpenAPI/components/preview/controllers/render-controller.ts
+++ src/view/OpenAPI/components/preview/controllers/render-controller.ts
@@ -1,27 +1,46 @@
 import OpenAPIPreviewController from 'view/OpenAPI/components/preview/controllers/preview-controller';
-import { RESOURCE_NAME } from '../../../../typing/constants';
+import { RESOURCE_NAME } from 'view/typing/constants';
 
 export class RenderController {
-    private ui: any;
+    ui: any;
     constructor(public controller: OpenAPIPreviewController) {}
 
+    /**
+     * Renders the Swagger UI.
+     *
+     * This method is called when the preview controller is initializes.
+     * It renders the Swagger UI for the preview component.
+     *
+     * @returns {Promise<void>} A promise that resolves when the Swagger UI bundle has been rendered.
+     */
     async render(): Promise<void> {
         const { contentEl } = this.controller.preview;
         await this.renderSwaggerUI(contentEl);
     }
 
-    async renderSwaggerUI(containerEl: HTMLDivElement): Promise<void> {
+    /**
+     * Renders the Swagger UI for the preview component.
+     *
+     * This method is responsible for rendering the Swagger UI.
+     *
+     * @param containerEl The HTML element to render the Swagger UI component into.
+     *
+     * @returns {Promise<void>} A promise that resolves when the Swagger UI component is rendered.
+     *
+     * @throws If there is an error loading the CSS resources.
+     */
+    private async renderSwaggerUI(containerEl: HTMLDivElement): Promise<void> {
         const { plugin } = this.controller.preview;
         if (!plugin.resourceManager.swaggerUIBundle) {
             await plugin.resourceManager.initSwaggerUIBundle();
         }
 
-        if (this.ui) {
-            const spec = await this.controller.previewUtils.loadSpec();
-            if (spec) {
-                this.ui.specActions.updateJsonSpec(spec);
-                return;
-            }
+        const updated = await this.updatePreview(
+            await this.controller.previewUtilController.loadSpec()
+        );
+
+        if (updated) {
+            return;
         }
 
         containerEl.empty();
@@ -45,7 +64,8 @@ export class RenderController {
             cls: 'fill-height-or-more',
         });
 
-        const parsedSpec = await this.controller.previewUtils.loadSpec();
+        const parsedSpec =
+            await this.controller.previewUtilController.loadSpec();
 
         if (!parsedSpec) {
             return;
@@ -67,8 +87,36 @@ export class RenderController {
         });
     }
 
+    /**
+     * Re-renders the preview.
+     *
+     * This method is called when the underlying specification changes.
+     * It resets the Swagger UI bundle and re-renders the preview.
+     *
+     * @returns {Promise<void>} A promise that resolves when the preview is re-rendered.
+     */
     async rerender(): Promise<void> {
         this.ui = null;
         await this.render();
+    }
+
+    /**
+     * Updates the preview with a new specification.
+     *
+     * This method is called when the specification file changes.
+     * It updates the Swagger UI bundle with the new specification.
+     *
+     * @param {object} spec - The new specification.
+     * @returns {Promise<boolean>} A promise that resolves to `true` if the
+     *          specification was updated successfully, or `false` otherwise.
+     */
+    async updatePreview(spec: any): Promise<boolean> {
+        if (this.ui) {
+            if (spec) {
+                this.ui.specActions.updateJsonSpec(spec);
+                return true;
+            }
+        }
+        return false;
     }
 }
