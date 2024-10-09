92a234517f07e567d7ab69885a0bf2e115629453
Sentiago <arseny317@gmail.com>
2024-10-09 23:38:25 +0300

    feat: move theme button creation to view and centralize theme switching management through common interfaces
    - Moved theme button creation exclusively to the view-side
    - Subscribed to `css-change` and `OpenAPIThemeChangeState` events to update the active component's theme

--- src/view/OpenAPI/controllers/view-controller.ts
+++ src/view/OpenAPI/controllers/view-controller.ts
@@ -1,19 +1,31 @@
-import { OpenAPIView } from 'view/OpenAPI/OpenAPI-view';
-import { eventID } from '../../../events-management/typing/constants';
+import { eventID } from 'events-management/typing/constants';
 import {
-    SwitchModeStateEvent,
+    OpenAPIThemeChangeState,
     UpdateOpenAPIViewStateEvent,
-} from '../../../events-management/typing/interfaces';
-import { RenderingMode } from '../../typing/constants';
+} from 'events-management/typing/interfaces';
+import { createNewLeaf, updateButton } from 'view/common/helpers';
+import { OpenAPIView } from 'view/OpenAPI/openapi-view';
+import { RenderingMode } from 'view/typing/constants';
 
-import { OPENAPI_VERSION_VIEW_TYPE } from '../../typing/types';
+import { OPENAPI_VERSION_VIEW } from 'view/typing/types';
+import { UtilController } from './util-controller';
 
 export class OpenAPIController {
+    utils: UtilController;
     constructor(public view: OpenAPIView) {
+        this.utils = new UtilController(this);
         this.initializeActions();
         this.setupEventListeners();
     }
 
+    /**
+     * The new mode that the view should be switched to.
+     * Given the current mode, it will return the other supported mode.
+     * Switches between Preview and Source modes
+     * If the current mode is not supported, it will throw an error.
+     *
+     * @returns {RenderingMode} The new mode to switch to.
+     */
     get newMode(): RenderingMode {
         switch (this.view.mode) {
             case RenderingMode.Preview:
@@ -21,108 +33,174 @@ export class OpenAPIController {
             case RenderingMode.Source:
                 return RenderingMode.Preview;
             default:
-                throw new Error('Unsupported mode');
+                throw new Error(`Unsupported mode: ${this.view.mode}`);
         }
     }
 
-    get modeIcon(): 'book-open' | 'pen' {
+    /**
+     * Returns the icon to use for the mode switch action.
+     *
+     * When the mode is Preview, the icon is 'book-open'. Otherwise, the icon is 'pen'.
+     *
+     * @returns {'book-open' | 'pen'} The icon to use.
+     */
+    get modeActionIcon(): 'book-open' | 'pen' {
         return this.view.mode === RenderingMode.Preview ? 'book-open' : 'pen';
     }
 
-    get modeTitle() {
+    /**
+     * The title to use for the mode switch action.
+     *
+     * Given the current mode, it will return a string that describes the current view
+     * and the action that will occur when the button is clicked.
+     *
+     * The string is formatted as follows:
+     *   Current view: [reading|editing]
+     *   Click to [edit|read]
+     *   Ctrl+Click to open to the right
+     *   Alt+Click to open in floating window
+     *
+     * @returns {string} The title to use.
+     */
+    get modeActionTitle(): string {
         const currentMode = this.view.mode;
-        return `Current view: ${currentMode === RenderingMode.Preview ? 'reading' : 'editing'}\nClick to ${currentMode === RenderingMode.Preview ? 'edit' : 'read'}\nCtrl+Click to open to the right\nAlt+Click to open in floating window`;
+        const currentViewMsg = `Current view: ${currentMode === RenderingMode.Preview ? 'reading' : 'editing'}`;
+        const modeMsg = `Click to ${currentMode === RenderingMode.Preview ? 'edit' : 'read'}`;
+        const ctrlAltMsg = `Ctrl+Click to open to the right\nAlt+Click to open in floating window`;
+        return `${currentViewMsg}\n${modeMsg}\n${ctrlAltMsg}`;
     }
 
-    createActions(): {
+    /**
+     * Initializes the actions for the OpenAPI view.
+     *
+     * The actions are:
+     *   1. Mode switch button: Switches between Preview and Source modes.
+     *      It will open a new leaf of the same type to the right if Ctrl is pressed,
+     *      and open a new leaf of the same type in a floating window if Alt is pressed.
+     *      Otherwise, it will switch the mode of the current leaf.
+     *   2. Version control button: Opens a new leaf of type OPENAPI_VERSION_VIEW_TYPE.
+     *   3. Theme button: Switches current active component between light and dark theme.
+     *
+     * @returns {Object} An object with the following keys:
+     *  - changeModeButton: The mode switch button.
+     *  - versionViewButton: The version control button.
+     *  - themeButton: The theme button.
+     */
+    initializeActions(): {
         versionViewButton: HTMLElement;
         changeModeButton: HTMLElement;
+        themeButton: HTMLElement;
     } {
         const changeModeButton = this.view.addAction(
-            this.modeIcon,
-            this.modeTitle,
+            this.modeActionIcon,
+            this.modeActionTitle,
             async (event: MouseEvent) => {
                 const oldState = this.view.leaf.getViewState();
                 if (event.ctrlKey) {
-                    const leaf = this.view.app.workspace.getLeaf('split');
-                    await leaf.setViewState({
-                        ...oldState,
-                    });
-                    leaf.setGroup('openapi-renderer-view-group');
-                    this.view.leaf.setGroupMember(leaf);
+                    await this.utils.openLeaf('split', oldState);
                 } else if (event.altKey) {
-                    const leaf = this.view.app.workspace.getLeaf('window');
-                    await leaf.setViewState({
-                        ...oldState,
-                    });
-                    leaf.setGroup('openapi-renderer-view-group');
-                    this.view.leaf.setGroupMember(leaf);
+                    await this.utils.openLeaf('window', oldState);
                 } else {
                     this.view.onSwitch();
                 }
+                updateButton(
+                    changeModeButton,
+                    this.modeActionIcon,
+                    this.modeActionTitle
+                );
             }
         );
+
         const versionViewButton = this.view.addAction(
-            'file-clock',
+            'git-compare-arrows',
             'Version control',
             async () => {
-                const file = this.view.file;
-                if (!file) {
-                    return;
-                }
-
-                const openAPIVersionLeaves =
-                    this.view.app.workspace.getLeavesOfType(
-                        OPENAPI_VERSION_VIEW_TYPE
-                    );
-
-                const existingView = openAPIVersionLeaves.find(
-                    (leaf) => leaf.getViewState().state.file === file.path
-                );
-
-                if (existingView) {
-                    const newViewState = {
-                        ...existingView.getViewState(),
-                        active: true,
-                    };
-                    await existingView.setViewState(newViewState);
-                } else {
-                    const newLeaf = this.view.app.workspace.getLeaf(true);
-                    await newLeaf.setViewState({
-                        type: OPENAPI_VERSION_VIEW_TYPE,
-                        active: true,
-                        state: {
-                            file: file.path,
-                        },
-                    });
-                }
+                await createNewLeaf(OPENAPI_VERSION_VIEW, this.view);
             }
         );
 
-        return { changeModeButton, versionViewButton };
-    }
+        const themeButton = this.view.addAction(
+            this.view.activeComponent?.controller.themeController
+                .themeButtonIcon ?? '',
+            `Theme: ${this.view.activeComponent?.themeMode}`,
+            async () => {
+                if (!this.view.activeComponent) {
+                    return;
+                }
+                this.view.activeComponent.controller.themeController.toggleThemeManually();
+                await this.view.activeComponent.controller.themeController.requestThemeUpdate();
+                updateButton(
+                    themeButton,
+                    this.view.activeComponent.controller.themeController
+                        .themeButtonIcon,
+                    `Theme: ${this.view.activeComponent.themeMode}`
+                );
+            }
+        );
 
-    initializeActions(): void {
-        let { changeModeButton, versionViewButton } = this.createActions();
         this.view.plugin.observer.subscribe(
             this.view.plugin.app.workspace,
             eventID.SwitchModeState,
-            async (event: SwitchModeStateEvent) => {
-                const leaf = event.data.leaf;
-                if (leaf === this.view.leaf) {
-                    changeModeButton.remove();
-                    versionViewButton.remove();
-                    const actions = this.createActions();
-                    changeModeButton = actions.changeModeButton;
-                    versionViewButton = actions.versionViewButton;
-                }
+            async () => {
+                updateButton(
+                    changeModeButton,
+                    this.modeActionIcon,
+                    this.modeActionTitle
+                );
             }
         );
+
+        this.view.plugin.observer.subscribe(
+            this.view.plugin.app.workspace,
+            eventID.SwitchModeState,
+            async () => {
+                updateButton(
+                    themeButton,
+                    this.view.activeComponent?.controller.themeController
+                        .themeButtonIcon,
+                    `Theme: ${this.view.activeComponent?.themeMode}`
+                );
+            }
+        );
+
+        this.view.plugin.observer.subscribe(
+            this.view.plugin.app.workspace,
+            eventID.OpenAPIThemeChangeState,
+            async (event: OpenAPIThemeChangeState) => {
+                const mode = event.data.mode as RenderingMode;
+                if (mode !== this.view.mode) {
+                    return;
+                }
+                updateButton(
+                    themeButton,
+                    this.view.activeComponent?.controller.themeController
+                        .themeButtonIcon,
+                    `Theme: ${this.view.activeComponent?.themeMode}`
+                );
+            }
+        );
+
+        return {
+            changeModeButton: changeModeButton,
+            versionViewButton: versionViewButton,
+            themeButton: themeButton,
+        };
     }
 
+    /**
+     * Sets up event listeners.
+     *
+     * This method sets up event listeners for:
+     *   1. Ctrl+E keyboard shortcut: Switches between Preview and Source modes.
+     *   2. `UpdateOpenAPIViewState` event: Updates the content of the view if the
+     *      file is the one currently being rendered and the mode is the same.
+     *   3. `css-change` event: Updates the theme of the view when the app theme changes.
+     *   4. `OpenAPIThemeChangeState` event: Updates the theme of the view when the theme
+     *      of another view changes and the mode is the same.
+     *
+     * @returns {void} No return value.
+     */
     setupEventListeners(): void {
-        this.view.containerEl.setAttr('tabindex', '0');
-        this.view.contentEl.setAttr('tabindex', '0');
         this.view.plugin.registerDomEvent(
             this.view.containerEl,
             'keyup',
@@ -142,16 +220,25 @@ export class OpenAPIController {
                         this.view.file
                     );
                     this.view.setViewData(content, false);
-                    this.view.plugin.publisher.publish({
-                        eventID: eventID.SwitchModeState,
-                        timestamp: new Date(),
-                        emitter: this.view.app.workspace,
-                        data: {
-                            leaf: this.view.leaf,
-                        },
-                    } as SwitchModeStateEvent);
-                    this.view.activeComponent.render();
+                    this.utils.publishSwitchEvent();
+                    this.view.activeComponent?.render();
                 }
+            }
+        );
+
+        this.view.app.workspace.on('css-change', async () => {
+            await this.view.activeComponent?.controller.themeController.themeUpdate();
+        });
+
+        this.view.plugin.observer.subscribe(
+            this.view.plugin.app.workspace,
+            eventID.OpenAPIThemeChangeState,
+            async (event: OpenAPIThemeChangeState) => {
+                const viewMode = event.data.mode as RenderingMode;
+                if (viewMode !== this.view.mode) {
+                    return;
+                }
+                await this.view.activeComponent?.controller.themeController.themeUpdate();
             }
         );
     }
