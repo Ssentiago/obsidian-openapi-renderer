740b62ae13104629dc7871b7da1a6997fb2494ba
Sentiago <arseny317@gmail.com>
2024-10-09 23:03:28 +0300

    refactor: update code due to changes in props

--- src/settings/components/settings-tab/SettingsTab.tsx
+++ src/settings/components/settings-tab/SettingsTab.tsx
@@ -1,6 +1,4 @@
-import { App } from 'obsidian';
 import React from 'react';
-import OpenAPIRendererPlugin from '../../../core/openapi-renderer-plugin';
 import { useSettingsContext } from '../core/context';
 import GeneralSection from '../general-section/GeneralSection';
 import Navbar from '../navbar/Navbar';
@@ -13,24 +11,19 @@ import OpenAPIViewSection from '../openapi-view-section/OpenAPIViewSection';
  * This component renders the main settings page for OpenAPI Renderer, including
  * the navbar and the content of the currently selected tab.
  *
- * @param app The Obsidian app instance.
- * @param plugin The OpenAPI Renderer plugin instance.
  * @returns A React element representing the main settings page.
  */
-const SettingsTab: React.FC<{
-    app: App;
-    plugin: OpenAPIRendererPlugin;
-}> = ({ app, plugin }): React.ReactElement => {
+const SettingsTab: React.FC = (): React.ReactElement => {
     const { currentTab } = useSettingsContext();
 
     const renderContent = () => {
         switch (currentTab) {
             case 'general':
-                return <GeneralSection app={app} plugin={plugin} />;
+                return <GeneralSection />;
             case 'openapi-view':
-                return <OpenAPIViewSection app={app} plugin={plugin} />;
+                return <OpenAPIViewSection />;
             case 'openapi-entry-view':
-                return <OpenAPIEntrySection app={app} plugin={plugin} />;
+                return <OpenAPIEntrySection />;
         }
     };
 
