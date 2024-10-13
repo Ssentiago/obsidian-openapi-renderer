9aa152d5c6c44ef0ac55780fe2ceb634b3f3c3e0
Sentiago <arseny317@gmail.com>
2024-10-13 03:00:27 +0300

    feat: update `start-obsidian` script

--- scripts/start-obsidian.ts
+++ scripts/start-obsidian.ts
@@ -1,4 +1,6 @@
-import {spawn} from 'child_process';
+import { spawn } from 'child_process';
+import { promises as fs } from 'node:fs';
+import * as process from 'node:process';
 import psList from 'ps-list';
 
 const OBSIDIAN_PATH = process.env.OBSIDIAN_PATH;
@@ -7,23 +9,56 @@ if (!OBSIDIAN_PATH) {
     console.error('OBSIDIAN_PATH is not set in the environment variables.');
     process.exit(1);
 }
+
+/**
+ * Checks if Obsidian is currently running.
+ *
+ * @returns A promise that resolves to `true` if Obsidian is running, `false` otherwise.
+ */
 async function isObsidianRunning() {
     const processes = await psList();
-    return processes.some(p => p.name.toLowerCase().includes('obsidian'));
+    return processes.some((p) => p.name.toLowerCase().includes('obsidian'));
 }
 
-async function startObsidian() {
+/**
+ * Starts Obsidian in debugging mode at port 9222.
+ *
+ * @param obsidianPath - The path to the Obsidian executable.
+ *
+ * @returns A promise that resolves when Obsidian has been started.
+ */
+async function startObsidian(obsidianPath: string) {
     const isRunning = await isObsidianRunning();
     if (isRunning) {
+        console.log('Obsidian is already running.');
         return;
     }
 
-    const cp = spawn(OBSIDIAN_PATH, ['--remote-debugging-port=9222'], {detached: true, stdio: 'ignore'});
+    try {
+        await fs.access(obsidianPath, fs.constants.F_OK);
+    } catch (err: any) {
+        if (err.code === 'ENOENT') {
+            console.error('Obsidian not found at path:', obsidianPath);
+            process.exit(1);
+        } else {
+            throw err;
+        }
+    }
+
+    const cp = spawn(obsidianPath, ['--remote-debugging-port=9222'], {
+        detached: true,
+        stdio: 'ignore',
+    });
     cp.unref();
     console.log('Obsidian started in debugging mode at port 9222.');
 }
 
-startObsidian().catch(err => {
+if (!OBSIDIAN_PATH) {
+    console.error('OBSIDIAN_PATH is not set in the environment variables.');
+    process.exit(1);
+}
+
+startObsidian(OBSIDIAN_PATH).catch((err) => {
     console.error('Error starting Obsidian:', err);
     process.exit(1);
 });
